f5d0dba969ef621b677a23db91fde6f1a89d43cc
Juan Lopez <juanquiroslopez@gmail.com>
2024-12-02 22:22:43 +0100

    feat: added Angular material dependency

--- smile-clinic-front/src/app/app.config.ts
+++ smile-clinic-front/src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app-routing.module';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(), provideAnimationsAsync(),
   ],
 };
