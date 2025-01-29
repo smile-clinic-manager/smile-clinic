1f77f8922ff16d5f2b32c78d60d68efa59223a1f
juanlo017 <54325276+juanlo017@users.noreply.github.com>
2025-01-29 16:55:17 +0100

    feat: Improved signUp/register endpoint (#82)
    Improved validation and business logic in the endpoint including customized error messages and exceptions
    such as check for repeated usernames, mismatched incoming passwords and runtime exceptions

--- smile-clinic-back/smile-clinic/src/main/java/com/smile/clinic/smile_clinic/application/services/UserService.java
+++ smile-clinic-back/smile-clinic/src/main/java/com/smile/clinic/smile_clinic/application/services/UserService.java
@@ -4,16 +4,17 @@ import com.smile.clinic.smile_clinic.application.ports.input.UserServicePort;
 import com.smile.clinic.smile_clinic.application.ports.output.PasswordEncoderPort;
 import com.smile.clinic.smile_clinic.application.ports.output.TokenProviderPort;
 import com.smile.clinic.smile_clinic.application.ports.output.UserPersistancePort;
+import com.smile.clinic.smile_clinic.domain.exceptions.InsecurePasswordException;
 import com.smile.clinic.smile_clinic.domain.exceptions.UsernameAlreadyExistsException;
 import com.smile.clinic.smile_clinic.domain.models.users.User;
 import com.smile.clinic.smile_clinic.domain.models.users.roles.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Service
@@ -23,6 +24,9 @@ public class UserService implements UserServicePort {
     private final UserPersistancePort userPersistancePort;
     private final PasswordEncoderPort passwordEncoderPort;
     private final TokenProviderPort tokenProviderPort;
+
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{12,}$";
+    private static final Pattern pattern = Pattern.compile(PASSWORD_REGEX);
 
     @Override
     public User findById(Long id) {
@@ -44,33 +48,46 @@ public class UserService implements UserServicePort {
     }
 
     @Override
-    public Map<User, String> register(User user, String password) {
+    public Map<User, String> register(User user, String password) throws Exception {
         // Validate if available username
         if (userPersistancePort.findUserByUsername(user.getUsername()).isPresent()) {
-            throw new UsernameAlreadyExistsException("Username is already taken.");
+            throw new UsernameAlreadyExistsException("Nombre de usuario ya está siendo utilizado");
         }
-        // TODO: Validate password if necessary
 
-        // Encode the password and create the User domain model
-        String encodedPassword = passwordEncoderPort.encode(password);
+        if(!this.isValidPassword(password)){
+            throw new InsecurePasswordException("Contraseña no válida, debe contener al menos 12 caracteres, una mayúscula, una minúscula y un caracter especial");
+        }
 
-        User userToRegister = User.builder()
-                .username(user.getUsername())
-                .firstName(user.getFirstName())
-                .lastName1(user.getLastName2())
-                .lastName2(user.getLastName2())
-                .dni(user.getDni())
-                .email(user.getEmail())
-                .role(Role.CLINIC_ADMIN) // Temporary until we implement the roles logic
-                .build();
+        try{
+            // Encode the password and create the User domain model
+            String encodedPassword = passwordEncoderPort.encode(password);
 
-        User savedUser = userPersistancePort.save(userToRegister, encodedPassword);
-        String token = tokenProviderPort.generateToken(savedUser);
+            User userToRegister = User.builder()
+                    .username(user.getUsername())
+                    .firstName(user.getFirstName())
+                    .lastName1(user.getLastName2())
+                    .lastName2(user.getLastName2())
+                    .dni(user.getDni())
+                    .email(user.getEmail())
+                    .role(Role.CLINIC_ADMIN) // Temporary until we implement the roles logic
+                    .password(encodedPassword)
+                    .build();
 
-        Map<User, String> userTokenMap = new HashMap<>();
-        userTokenMap.put(savedUser, token);
+            User savedUser = userPersistancePort.save(userToRegister);
+            String token = tokenProviderPort.generateToken(savedUser);
 
-        return userTokenMap;
+            return Map.of(savedUser, token);
+
+        } catch (Exception e){
+            log.error(e.getMessage());
+            throw new Exception("Ha ocurrido un error al registrar el usuario");
+        }
+
+    }
+
+    private boolean isValidPassword(String password){
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 
     @Override
