a4bf58ad87924b8d64a0e4481410f36132303d81
juanlo017 <54325276+juanlo017@users.noreply.github.com>
2025-02-16 18:25:33 +0100

    Feature/r 046 header app (#101)
    * fix: arreglar validators and minor fixes
    
    Added and fixed validators in the signup form, fixed minor compile issue in the backend.
    Pending validators; repeated password and repeated email.
    
    * feat: Added mismatched form value validator
    
    Added mismatched form value validator for both email & password form fields
    
    * feat: Added new app layout UI
    
    * feat: auth guard, styles and layout app
    
    added auth guard to protect logged routes, changed styles css and configured the layoutapp across the application
    
    * fix: modified styles, fixed router
    
    Modified the router to show the current component within the layout and added color to the header bar

--- smile-clinic-front/smile-clinic/src/app/app.routes.ts
+++ smile-clinic-front/smile-clinic/src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.c
 import { LayoutComponent } from './modules/layout/layout.component';
 import { LoginComponent } from './modules/login/login.component';
 import { SignUpComponent } from './modules/signup/signup.component';
+import { HomeComponent } from './modules/home/home.component';
+import { authGuard } from './auth.guard';
 import { ClinicListComponent } from './modules/clinic-list/clinic-list.component';
+
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'layout', component: LayoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignUpComponent },
-  { path: 'clinic-list', component : ClinicListComponent },
-  { path: '', component: PageNotFoundComponent }, //wildcard, when none route is matched, this one triggers (404 err)
+  { path: '', component: LayoutComponent, canActivate: [authGuard],
+    children: [
+      { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+      { path: 'clinic-list', component : ClinicListComponent, canActivate: [authGuard] },
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent } // Wildcard route (404 page)
 ];
