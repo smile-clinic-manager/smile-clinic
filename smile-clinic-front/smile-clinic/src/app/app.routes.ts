4469b013943d342eec2ff603778b619e03193303
Jose Castro <64309745+joscasvaz@users.noreply.github.com>
2025-02-20 20:30:59 +0100

    feat: list treatments
    Still have to check out the backend to see how DTO is coming from the endpoint.
    Also made minor fixes and refactoring.

--- smile-clinic-front/smile-clinic/src/app/app.routes.ts
+++ smile-clinic-front/smile-clinic/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.c
 import { LayoutComponent } from './modules/layout/layout.component';
 import { LoginComponent } from './modules/login/login.component';
 import { SignUpComponent } from './modules/signup/signup.component';
+import { TreatmentListComponent } from './modules/treatment-list/treatment-list.component';
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'layout', component: LayoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: SignUpComponent },
+  { path: 'treatments-list', component: TreatmentListComponent },
   { path: '', component: PageNotFoundComponent }, //wildcard, when none route is matched, this one triggers (404 err)
 ];
