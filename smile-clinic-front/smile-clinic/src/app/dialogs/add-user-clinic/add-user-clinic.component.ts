3033a8748bbff4d77add053261e9836bbff7ae03
Juan López Quirós <jualopqui1@alum.us.es>
2025-03-11 18:27:23 +0100

    fix: mini fix styles

--- smile-clinic-front/smile-clinic/src/app/dialogs/add-user-clinic/add-user-clinic.component.ts
+++ smile-clinic-front/smile-clinic/src/app/dialogs/add-user-clinic/add-user-clinic.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -17,11 +17,15 @@ export class AddUserClinicComponent {
 
   constructor(private userService: UserService, private snackBarService: SnackbarServiceService) {  }
   foundUser: any = undefined; // TODO: Asignar tipos de dato a foundUser
-  searchUserForm = new FormControl('', Validators.required)
+  
+  userSearchForm = new FormGroup({
+    idUser: new FormControl('', Validators.required),
+  })
 
   findUser() {
     this.userService.getUserById('2').then((user) => {
       this.foundUser = user;
+      console.log(this.foundUser);
     })
     .catch(() => this.snackBarService.showErrorSnackBar('Usuario no encontrado'))
   }
