74f1ff7ddf038be763ce815b4b64fffaf920255b
Jose Castro <64309745+joscasvaz@users.noreply.github.com>
2025-03-13 17:32:45 +0100

    feat: clinic form
    Made a new component to handle clinics as a form -for creating and updating as well-

---
+++ smile-clinic-front/smile-clinic/src/app/modules/clinics/clinic-form/clinic-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clinic-form',
+  imports: [],
+  templateUrl: './clinic-form.component.html',
+  styleUrl: './clinic-form.component.scss'
+})
+export class ClinicFormComponent {
+
+}
