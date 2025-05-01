2c99d1b10b8b1b462bf9e24394688afaf3cf34d0
Jose Castro <64309745+joscasvaz@users.noreply.github.com>
2025-05-01 18:10:08 +0200

    feat: missing backend TreatmentInstance classes

---
+++ smile-clinic-back/smile-clinic/src/main/java/com/smile/clinic/smile_clinic/infrastructure/adapters/output/persistance/adapters/TreatmentInstancePersistanceAdapter.java
@@ -0,0 +1,49 @@
+package com.smile.clinic.smile_clinic.infrastructure.adapters.output.persistance.adapters;
+
+import com.smile.clinic.smile_clinic.application.ports.output.TreatmentInstancePersistancePort;
+import com.smile.clinic.smile_clinic.domain.models.TreatmentInstance;
+import com.smile.clinic.smile_clinic.infrastructure.adapters.output.persistance.mappers.TreatmentInstancePersistanceMapper;
+import com.smile.clinic.smile_clinic.infrastructure.adapters.output.persistance.repositories.TreatmentInstanceEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TreatmentInstancePersistanceAdapter implements TreatmentInstancePersistancePort {
+
+    private final TreatmentInstanceEntityRepository treatmentInstanceEntityRepository;
+    private final TreatmentInstancePersistanceMapper treatmentInstanceMapper;
+    private final TreatmentInstanceEntityRepository treatmentInstanceEntityMapper;
+
+    @Override
+    public TreatmentInstance findById(Long id) {
+        return treatmentInstanceMapper.toTreatmentInstance(treatmentInstanceEntityRepository.findById(id).orElse(null));
+    }
+
+    @Override
+    public List<TreatmentInstance> findAll() {
+        return treatmentInstanceMapper.toTreatmentInstanceList(treatmentInstanceEntityRepository.findAll());
+    }
+
+    @Override
+    public List<TreatmentInstance> findByPatientId(Long id) {
+        return treatmentInstanceMapper.toTreatmentInstanceList(treatmentInstanceEntityRepository.findByPatientId(id));
+    }
+
+    @Override
+    public TreatmentInstance save(TreatmentInstance treatmentInstance) {
+        return treatmentInstanceMapper.toTreatmentInstance(treatmentInstanceEntityRepository.save(treatmentInstanceMapper.toTreatmentInstanceEntity(treatmentInstance)));
+    }
+
+    @Override
+    public TreatmentInstance update(Long id, TreatmentInstance treatmentInstance) {
+        return treatmentInstanceMapper.toTreatmentInstance(treatmentInstanceEntityRepository.save(treatmentInstanceMapper.toTreatmentInstanceEntity(treatmentInstance)));
+    }
+
+    @Override
+    public void delete(TreatmentInstance treatmentInstance) {
+        treatmentInstanceEntityRepository.delete(treatmentInstanceMapper.toTreatmentInstanceEntity(treatmentInstance));
+    }
+}
