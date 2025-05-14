81ccee095e9cfa944f306a7898bdd0d221ee3fbb
Juan López Quirós <jualopqui1@alum.us.es>
2025-05-15 00:11:04 +0200

    feat: primera version odontograma front

--- smile-clinic-front/smile-clinic/src/app/modules/odontogram/odontogram.component.ts
+++ smile-clinic-front/smile-clinic/src/app/modules/odontogram/odontogram.component.ts
@@ -3,10 +3,12 @@ import { TeethDTO } from '../../models/TeethDTO';
 import { OdontogramService } from '../../../services/odontogram.service';
 import { MedicalHistoryDTO } from '../../models/MedicalHistoryDTO';
 import { SnackbarServiceService } from '../../../services/snackbar-service.service';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-odontogram',
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './odontogram.component.html',
   styleUrl: './odontogram.component.scss'
 })
@@ -14,16 +16,34 @@ export class OdontogramComponent implements OnInit{
   @Input() medicalHistoryDTO: MedicalHistoryDTO | undefined = undefined;
   teethList: TeethDTO[] = [];
 
+  rows: TeethDTO[][] = [];
+
   constructor(private odontogramService :OdontogramService, private snackBarService: SnackbarServiceService){}
 
-  ngOnInit(): void {
-    console.log(this.medicalHistoryDTO);
+  async ngOnInit(): Promise<void> {
+    await this.getOdontogramData();
+
+    
+  }
+
+  async getOdontogramData(): Promise<void>{
     this.odontogramService.getAllTeeth(this.medicalHistoryDTO!.id)
       .then((teethList)=>{
         this.teethList = teethList;
         console.log('this.teethList');
         console.log(this.teethList);
       })
+      .finally(() => this.buildOdontogramLayout())
       .catch(e=>this.snackBarService.showErrorSnackBar("Error al obtener el odontograma"));
   }
+
+  buildOdontogramLayout(): void{
+    const teethRowLength = 16;
+    for (let i = 0; i < this.teethList.length; i += teethRowLength) {
+      this.rows.push(this.teethList.slice(i, i + teethRowLength));
+    }
+    console.log('this.rows');
+    console.log(this.rows);
+  }
+
 }
