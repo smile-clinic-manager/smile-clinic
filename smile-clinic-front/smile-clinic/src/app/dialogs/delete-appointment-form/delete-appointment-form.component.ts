424b1007fe2afb3f40fed3a6348e102aabab8f03
Juan López Quirós <jualopqui1@alum.us.es>
2025-05-30 19:02:05 +0200

    fix delete appointment

--- smile-clinic-front/smile-clinic/src/app/dialogs/delete-appointment-form/delete-appointment-form.component.ts
+++ smile-clinic-front/smile-clinic/src/app/dialogs/delete-appointment-form/delete-appointment-form.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { AppointmentDTO } from '../../models/AppointmentDTO';
 
 @Component({
   selector: 'app-delete-appointment-form',
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatTimepickerModule, MatOptionModule, MatDialogModule, MatButtonModule],
   templateUrl: './delete-appointment-form.component.html',
   styleUrl: './delete-appointment-form.component.scss'
 })
 export class DeleteAppointmentFormComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {appointment: AppointmentDTO},
+   private dialogRef: MatDialogRef<DeleteAppointmentFormComponent>){ }
+
+
+  deleteAppointment(): void{
+
+  }
+
+  closeDialog(): void{
+    
+  }
 }
